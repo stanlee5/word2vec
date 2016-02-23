7f42e23ae7ecb1b76acc8df3f7b725666dc38ec8
stanlee5 <stanlee5@naver.com>
2016-02-23 20:39:15 +0900

    start

--- word2vec.c
+++ word2vec.c
@@ -12,6 +12,8 @@
 //  See the License for the specific language governing permissions and
 //  limitations under the License.
 
+//  Edited by stanlee5
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
