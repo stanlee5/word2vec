84b91743a5720cee863608fcfeadec87ee09c3a9
serverCom <stanlee5@naver.com>
2016-03-08 11:24:39 +0900

    distance

--- distance.c
+++ distance.c
@@ -12,6 +12,7 @@
 //  See the License for the specific language governing permissions and
 //  limitations under the License.
 
+// stanlee5
 #include <stdio.h>
 #include <string.h>
 #include <math.h>
